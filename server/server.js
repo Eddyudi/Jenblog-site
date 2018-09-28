b76854f9c2e1150ed5f66ab0d9a498a6395b5c82
Eddy <eddyudi89@gmail.com>
2018-09-28 11:59:57 +0100

    commit update

--- server/server.js
+++ server/server.js
@@ -2,10 +2,11 @@ const path = require('path');
 const express = require('express');
 
 const pubPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
 
 app.use(express.static(pubPath));
 
-app.listen(3000, () =>{
-    console.log("server is running on Port 3000.");
+app.listen(port, () =>{
+    console.log(`server is running on ${port}.`);
 });
