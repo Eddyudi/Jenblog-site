e6304854017cbc5c181eeeaea6cf1009c7768240
Eddy <eddyudi89@gmail.com>
2018-09-28 12:45:49 +0100

    added sockets handlers

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const pubPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(pubPath));
 
-app.listen(port, () =>{
+io.on('connection', (socket)=> {
+
+    socket.on('disconnect', () =>{
+        console.log('User disconnected');
+    });
+});
+
+server.listen(port, () =>{
     console.log(`server is running on ${port}.`);
 });
